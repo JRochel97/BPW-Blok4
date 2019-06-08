f49c57301c351519a826b3f025f2422b9aa78f05
JRochel <jrochel97@gmail.com>
2019-06-08 13:22:25 +0200

    Made a transaction for the endscreen

---
+++ Assets/Scrips/EndscreenComplete.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndscreenComplete : MonoBehaviour
+{
+    public void BacktoTheBeginnin()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
