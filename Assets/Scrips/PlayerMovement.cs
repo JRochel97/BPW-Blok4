7bc421b39755fa543574998e43a9fb3971f19131
JRochel <jrochel97@gmail.com>
2019-06-20 17:03:26 +0200

    Made a prefab of the player. Added the run animation and transiction between idle and run. Also made it so the player flips when walking the other diraction.

--- Assets/Scrips/PlayerMovement.cs
+++ Assets/Scrips/PlayerMovement.cs
@@ -6,12 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 20f;
     public bool isGrounded = false;
+
+    private bool facingRight;
+
+    public Animator anim;
 
     void Start()
     {
-        
+        facingRight = true;
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -19,6 +24,27 @@ public class PlayerMovement : MonoBehaviour
         Jump();
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+           anim.SetBool("isRunning", true);
+        } else {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x = -1;
+
+            transform.localScale = theScale;
+        } if (Input.GetKeyDown(KeyCode.D))
+        {
+            Vector3 theScale = transform.localScale;
+            theScale.x = 1;
+
+            transform.localScale = theScale;
+        }
     }
 
     void Jump()
@@ -30,4 +56,4 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-}
+  }
