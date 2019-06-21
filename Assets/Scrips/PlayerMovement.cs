a03245945ddf99d1f5f65b1b9695c70b49cfea80
JRochel <jrochel97@gmail.com>
2019-06-21 12:35:05 +0200

    Added a jump animation.

--- Assets/Scrips/PlayerMovement.cs
+++ Assets/Scrips/PlayerMovement.cs
@@ -9,13 +9,10 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 20f;
     public bool isGrounded = false;
 
-    private bool facingRight;
-
     public Animator anim;
 
     void Start()
     {
-        facingRight = true;
         anim = GetComponent<Animator>();
     }
 
@@ -25,20 +22,20 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        if (Input.GetKey("left") || Input.GetKey("right") || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
            anim.SetBool("isRunning", true);
         } else {
             anim.SetBool("isRunning", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown("left"))
         {
             Vector3 theScale = transform.localScale;
             theScale.x = -1;
 
             transform.localScale = theScale;
-        } if (Input.GetKeyDown(KeyCode.D))
+        } if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown("right"))
         {
             Vector3 theScale = transform.localScale;
             theScale.x = 1;
@@ -50,8 +47,14 @@ public class PlayerMovement : MonoBehaviour
     void Jump()
     {
         if (Input.GetButtonDown("Jump") && isGrounded == true)
-        {
+        { 
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 7f), ForceMode2D.Impulse);
+        }
+        if (isGrounded == true)
+        {
+            anim.SetBool("isJumping", false);
+        } else {
+            anim.SetBool("isJumping", true);
         }
         
     }
